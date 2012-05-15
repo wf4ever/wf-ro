716968ff910de55d67cd9168e00cc63a9a0220c0
piotrholubowicz <piotrhol@man.poznan.pl>
2012-05-15 15:50:20 +0200

    WFE-461: added more empty methods.

---
+++ src/main/java/org/purl/wf4ever/wf2ro/Wf2ROConverter.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package org.purl.wf4ever.wf2ro;
+
+import java.net.URI;
+import java.util.UUID;
+
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Workflow;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+/**
+ * @author piotrekhol
+ * 
+ */
+public abstract class Wf2ROConverter
+{
+
+	public void convert(WorkflowBundle wfbundle)
+	{
+		for (Workflow workflow : wfbundle.getWorkflows()) {
+			URI wfURI = workflow.getWorkflowIdentifier();
+			UUID wfUUID = UUID.fromString(wfURI.resolve("..").relativize(wfURI).toString());
+			URI rodlWfURI = addWorkflow(wfUUID);
+			deleteServiceAnnotations(rodlWfURI);
+			addWfDescAnnotation(workflow, rodlWfURI);
+			addRoEvoAnnotation(workflow, rodlWfURI);
+		}
+	}
+
+
+	protected void addRoEvoAnnotation(Workflow workflow, URI rodlWfURI)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+
+	protected void addWfDescAnnotation(Workflow workflow, URI rodlWfURI)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+
+	protected void deleteServiceAnnotations(URI rodlWfURI)
+	{
+		OntModel manifest = createManifestModel();
+	}
+
+
+	protected abstract URI addWorkflow(UUID wfUUID);
+
+
+	protected abstract OntModel createManifestModel();
+}
---
+++ src/test/java/org/purl/wf4ever/wf2ro/MockupWf2ROConverter.java
@@ -0,0 +1,27 @@
+package org.purl.wf4ever.wf2ro;
+
+import java.net.URI;
+import java.util.UUID;
+
+import com.hp.hpl.jena.ontology.OntModel;
+
+public class MockupWf2ROConverter
+	extends Wf2ROConverter
+{
+
+	@Override
+	public OntModel createManifestModel()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	protected URI addWorkflow(UUID wfUUID)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
