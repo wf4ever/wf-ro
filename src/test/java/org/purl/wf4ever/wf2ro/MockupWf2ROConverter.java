3af745aab51e4269862c2774c58beebe8e2d37be
Piotr Hołubowicz <piotrhol@man.poznan.pl>
2012-05-16 17:15:36 +0200

    WFE-461: added a conversion test

--- src/test/java/org/purl/wf4ever/wf2ro/MockupWf2ROConverter.java
+++ src/test/java/org/purl/wf4ever/wf2ro/MockupWf2ROConverter.java
@@ -1,52 +1,98 @@
 package org.purl.wf4ever.wf2ro;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
+import org.apache.log4j.Logger;
+import org.purl.wf4ever.rosrs.client.common.Vocab;
+
+import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+import uk.org.taverna.scufl2.api.io.WriterException;
+import uk.org.taverna.scufl2.rdfxml.RDFXMLReader;
+
+import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class MockupWf2ROConverter
 	extends Wf2ROConverter
 {
 
+	private static final Logger log = Logger.getLogger(MockupWf2ROConverter.class);
+
+	private Map<URI, OutputStream> resources = new HashMap<>();
+
+	private WorkflowBundleIO io = new WorkflowBundleIO();
+
+	private OntModel manifest = ModelFactory.createOntologyModel(OntModelSpec.OWL_LITE_MEM);
+
+	public static final URI roURI = URI.create("http://example.org/ROs/ro1/");
+
+
+	public MockupWf2ROConverter(URI serviceURI)
+	{
+		super(serviceURI);
+		Individual ro = manifest.createIndividual(roURI.toString(), Vocab.researchObject);
+		Individual m = manifest.createIndividual(roURI.resolve(".ro/manifest.rdf").toString(), Vocab.researchObject);
+		m.addProperty(Vocab.describes, ro);
+	}
+
+
 	@Override
 	protected OutputStream createAnnotationBodyOutputStream(URI annotationBodyURI)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		resources.put(annotationBodyURI, new ByteArrayOutputStream());
+		return resources.get(annotationBodyURI);
 	}
 
 
 	@Override
 	protected OntModel createManifestModel(URI roURI)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return manifest;
 	}
 
 
 	@Override
 	protected URI createResearchObject(UUID wfUUID)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return roURI;
 	}
 
 
 	@Override
-	protected URI addWorkflowBundle(URI roURI, UUID wfUUID)
+	protected URI addWorkflowBundle(URI roURI, WorkflowBundle wfbundle, UUID wfUUID)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		URI wfURI = roURI.resolve(wfUUID.toString());
+		OutputStream out = new ByteArrayOutputStream();
+		try {
+			io.writeBundle(wfbundle, out, RDFXMLReader.APPLICATION_VND_TAVERNA_SCUFL2_WORKFLOW_BUNDLE);
+		}
+		catch (WriterException | IOException e) {
+			log.error("Can't save the workflow bundle", e);
+		}
+		resources.put(wfURI, out);
+		return wfURI;
 	}
 
 
 	@Override
 	protected void uploadManifest(URI roURI, OntModel manifest)
 	{
-		// TODO Auto-generated method stub
+		this.manifest = manifest;
+	}
 
+
+	public Map<URI, OutputStream> getResources()
+	{
+		return resources;
 	}
 
 }
--- src/test/java/org/purl/wf4ever/wf2ro/Wf2RoConverterTest.java
+++ src/test/java/org/purl/wf4ever/wf2ro/Wf2RoConverterTest.java
@@ -2,29 +2,75 @@ package org.purl.wf4ever.wf2ro;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
+import org.purl.wf4ever.rosrs.client.common.Vocab;
 
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.vocabulary.DCTerms;
 
 public class Wf2RoConverterTest
 {
 
 	private static final String helloWorldT2Flow = "helloworld.t2flow";
 
+	private static final String WF_UUID = "8781d5f4-d0ba-48a8-a1d1-14281bd8a917";
+
+	private static final URI serviceURI = URI.create("http://transformers");
+
 
 	@Test
 	public void testConvert()
-		throws ReaderException, IOException
+		throws ReaderException, IOException, URISyntaxException
 	{
 		WorkflowBundleIO io = new WorkflowBundleIO();
 		InputStream helloWorld = getClass().getClassLoader().getResourceAsStream(helloWorldT2Flow);
 		WorkflowBundle wfbundle = io.readBundle(helloWorld, null);
 
-		Wf2ROConverter converter = new MockupWf2ROConverter();
+		Wf2ROConverter converter = new MockupWf2ROConverter(serviceURI);
 		converter.convert(wfbundle);
-	}
 
+		URI wfURI = converter.createResearchObject(null).resolve(WF_UUID);
+
+		OntModel model = converter.createManifestModel(null);
+		Individual ro = model.getIndividual(converter.createResearchObject(null).toString());
+		Assert.assertNotNull("RO exists in the manifest", ro);
+		// should aggregate the workflow and 2 annotations about it
+		NodeIterator it = ro.listPropertyValues(Vocab.aggregates);
+		for (int i = 0; i < 3; i++) {
+			Assert.assertTrue("RO aggregates 3x wf or annotation", it.hasNext());
+			RDFNode node = it.next();
+			Assert.assertTrue(node.isURIResource());
+			Individual ind = node.as(Individual.class);
+			Assert.assertTrue("Wf or annotation",
+				ind.hasRDFType(Vocab.roResource) || ind.hasRDFType(Vocab.aggregatedAnnotation));
+			if (ind.hasRDFType(Vocab.roResource)) {
+				Assert.assertEquals("Wf URI is correct", wfURI.toString(), ind.getURI());
+				//TODO validate that the serialized wf is correct
+			}
+			else {
+				RDFNode targetNode = ind.getPropertyValue(Vocab.annotatesAggregatedResource);
+				Assert.assertTrue(targetNode.isURIResource());
+				Assert.assertEquals("Annotation target URI is correct", wfURI.toString(), targetNode.asResource()
+						.getURI());
+				RDFNode authorNode = ind.getPropertyValue(DCTerms.creator);
+				Assert.assertTrue(authorNode.isURIResource());
+				Assert.assertEquals("Annotation creator URI is correct", serviceURI.toString(), authorNode.asResource()
+						.getURI());
+				//TODO verify that the annotation body is correct
+			}
+		}
+		Assert.assertFalse("RO aggregates 3x wf or annotation", it.hasNext());
+	}
 }
